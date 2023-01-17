7169ab0810095f5a1c2163da9e0dd02f362c755f
E.Sabathier <47697572+emi-sabathier@users.noreply.github.com>
2023-01-17 11:54:28 +0100

    feature: add toast - fix TS - 3 team mate limit - readme - responsive

--- src/hooks/useDocumentTitle.tsx
+++ src/hooks/useDocumentTitle.tsx
@@ -3,9 +3,7 @@ import { dictionary } from '../assets/dictionary';
 
 export function useDocumentTitle(title: string): void {
     useEffect(() => {
-        console.log(title);
         if (title.includes('undefined')) {
-            console.log('jpp');
             document.title = `${dictionary.error.unknownMember}`;
         } else {
             document.title = title;
